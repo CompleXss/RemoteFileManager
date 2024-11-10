7ac39de2a3acd1cb4ad4693833c154f4a7295bf9
CompleXss <mp.maksim30@gmail.com>
2024-11-10 21:34:02 +0300

    rename folder

--- RemoteFileManager/wwwroot/js/exportGlobals.js
+++ RemoteFileManager/wwwroot/js/exportGlobals.js
@@ -1,6 +1,6 @@
 import {toggleTheme} from "./site.js";
-import {exportGlobals_AppRunner} from "./app_runner/exportGlobals.js";
-import {exportGlobals_FileManager} from "./file_manager/exportGlobals.js";
+import {exportGlobals_AppRunner} from "./appRunner/exportGlobals.js";
+import {exportGlobals_FileManager} from "./fileManager/exportGlobals.js";
 
 window.toggleTheme = toggleTheme
 
